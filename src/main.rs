09a6f5b28fdf3992cc6a963ada476bf2df16f8ee
Arto Bendiken <arto@asimov.so>
2025-05-23 12:16:34 +0400

    Configure tracing & logging.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 // This is free and unencumbered software released into the public domain.
 
-fn main() -> Result<(), ()> {
+#[cfg(feature = "std")]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Configure tracing & logging:
+    #[cfg(feature = "tracing")]
+    tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
+        .with_max_level(tracing_subscriber::filter::LevelFilter::WARN)
+        .init();
+
     println!("asimov-template-emitter"); // TODO
+
     Ok(())
+}
+
+#[cfg(not(feature = "std"))]
+fn main() {
+    unimplemented!("asimov-template-emitter requires the 'std' feature")
 }
